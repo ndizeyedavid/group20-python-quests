29f68e4e2b61af0824d18d1285ee8b76fb97d93f
pmadit-blip <p.madit@alustudent.com>
2026-06-28 09:47:50 -0700

    Add quest 11 guardian of the bridge

---
+++ quests/quest_11_guardian_of_the_bridge.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+age = int(input(Enter your age: "))
+    if age >= 18:
+    print("You are old enough to vote!")
+    else:
+    print("Sorry, you are not old enough to vote yet.")
