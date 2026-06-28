7731d406f57026baaa8e3646210ffb0847b8b44b
jngoh-maker <j.ngoh@alustudent.com>
2026-06-28 13:32:09 -0700

    Completing Quest 23. calculating area function

---
+++ quests/quest_23_oracles_vision.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def calculate_area(length, width):
+    return length * width
+
+area1 = calculate_area(5, 4)
+area2 = calculate_area(8, 6)
+
+print("Area of rectangle 1:", area1)
+print("Area of rectangle 2:", area2)
