b04e26c845c3325798e42791bb80f76eeb2233ef
pmadit-blip <p.madit@alustudent.com>
2026-06-29 12:48:59 -0700

    Fixed the issues that caused conflict

--- quests_11_guardian_of_the_bridge.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-age = int(input(Enter your age: "))
-    if age >= 18:
-    print("Your are old enough to vote!")
-    else:
-    ptint("Sorry, you are not old enough to vote yet.")
