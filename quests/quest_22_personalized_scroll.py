5da57944ca405c9ff82e0400b64de3de9ed6bf63
jngoh-maker <j.ngoh@alustudent.com>
2026-06-29 01:49:43 -0700

    changed Quest 22 to return greeting instead of printing

--- quests/quest_22_personalized_scroll.py
+++ quests/quest_22_personalized_scroll.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def personalized_greeting(name, quest):
-    print(f"Hello, {name}! Good luck on your quest to {quest}!")
+    """Return a personalized greeting."""
+    return f"Hello, {name}! Good luck on your quest to {quest}!"
+
 
 name = input("Enter your name: ")
 quest = input("Enter your quest: ")
 
-personalized_greeting(name, quest)
+print(personalized_greeting(name, quest))
