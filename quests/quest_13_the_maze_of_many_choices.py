1bd4f0eb7aac972ab15df36447f03e1c2e43c93f
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-06-29 12:03:44 +0200

    fix: veriffying that the number is in the range of 0-100

--- quests/quest_13_the_maze_of_many_choices.py
+++ quests/quest_13_the_maze_of_many_choices.py
@@ -6,13 +6,15 @@ if not score.isdigit():
 else:
     score = int(score)
     
-    if score >=90:
-        result = "A"
-    elif score >= 80:
-        result = "B"
-    elif score >= 70:
-        result = "C"
+    if score > 100 or score < 0:
+        result = "Enter a number between 0 and 100"
     else:
-        result = "Needs Improvement"
-
+        if score >=90:
+            result = "A"
+        elif score >= 80:
+            result = "B"
+        elif score >= 70:
+            result = "C"
+        else:
+            result = "Needs Improvement"
     print(result)
